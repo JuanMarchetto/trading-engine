040d0be5e716c51a94c0b648f6a26d1c90a9c724
Juan Marchetto <juanpatriciomarchetto@gmail.com>
2021-11-10 08:20:52 -0300

    fix logical error

--- src/handle_orders.rs
+++ src/handle_orders.rs
@@ -9,22 +9,20 @@ pub fn handle_orders(orders: Vec<Order>) -> (Orderbook, Vec<Trade>) {
     orders.into_iter().for_each(|order| {
         match order.side.as_str() {
             "SELL" => {
-                // IF there is a buy order with the same price or higher,
-                // then we have a trade
+                // IF there is a buy order with the same limit_price or higher,
+                // then we have a trade between the two orders
+                // and we remove the buy order from the orderbook
                 // else we add the order to the sells side of the orderbook
-                let mut found_buy = false;
-                for buy in orderbook.buys.iter() {
-                    if buy.limit_price >= order.limit_price {
-                        found_buy = true;
-                        break;
-                    }
-                }
-                if found_buy {
-                    let trade = Trade {
+                if let Some(buy_index) = orderbook
+                    .buys
+                    .iter()
+                    .position(|buy_order| buy_order.limit_price >= order.limit_price)
+                {
+                    let buy_order = orderbook.buys.remove(buy_index);
+                    trades.push(Trade {
                         sell: order,
-                        buy: orderbook.buys.pop().unwrap(),
-                    };
-                    trades.push(trade);
+                        buy: buy_order,
+                    });
                 } else {
                     orderbook.sells.push(order);
                 }
@@ -33,19 +31,16 @@ pub fn handle_orders(orders: Vec<Order>) -> (Orderbook, Vec<Trade>) {
                 // IF there is a sell order with the same price or lower,
                 // then we have a trade
                 // else we add the order to the buys side of the orderbook
-                let mut found_sell = false;
-                for sell in orderbook.sells.iter() {
-                    if sell.limit_price <= order.limit_price {
-                        found_sell = true;
-                        break;
-                    }
-                }
-                if found_sell {
-                    let trade = Trade {
-                        sell: orderbook.sells.pop().unwrap(),
+                if let Some(sell_index) = orderbook
+                    .sells
+                    .iter()
+                    .position(|sell_order| sell_order.limit_price <= order.limit_price)
+                {
+                    let sell_order = orderbook.sells.remove(sell_index);
+                    trades.push(Trade {
+                        sell: sell_order,
                         buy: order,
-                    };
-                    trades.push(trade);
+                    });
                 } else {
                     orderbook.buys.push(order);
                 }
